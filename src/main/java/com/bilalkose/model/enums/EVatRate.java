838bfd72498c57a859af08f8aa6a4715cc75a248
Bilal <bilalmuzafferkose@gmail.com>
2023-02-27 05:25:05 +0300

    todo modified

--- src/main/java/com/bilalkose/model/enums/EVatRate.java
+++ src/main/java/com/bilalkose/model/enums/EVatRate.java
@@ -2,8 +2,4 @@ package com.bilalkose.model.enums;
 
 public enum EVatRate {
     NONE, VERY_LOW, LOW, MEDIUM, HIGH, VERY_HIGH
-
-    //TODO: VatRate güncellemeleri
-    //TODO: sisteme Set eklenmesi
-    //TODO: daha fazla yorum satırları
 }
